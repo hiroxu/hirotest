b03b298aac6051b23219f205581e49b6b75d29b7
hiroxu <uesugi0519@gmail.com>
2017-02-07 20:25:00 +0800

    Add files via upload

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -2,7 +2,14 @@
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 var request = require('superagent');
-var LineBot = require('line-bot-sdk');
+//var LineBot = require('line-bot-sdk');
+const line = require('node-line-bot-api')
+line.init({
+    accessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU=',
+    // (Optional) for webhook signature validation
+    channelSecret: '6d53032b1569c18e7288cc431707aab4'
+})
+
 //var client = LineBot.client({
 //    channelID: '1499888713',
 //    channelSecret: '6d53032b1569c18e7288cc431707aab4',
@@ -10,11 +17,11 @@ var LineBot = require('line-bot-sdk');
 //});
 
 
-var bot = linebot({
-    channelId: '1499888713',
-    channelSecret: '6d53032b1569c18e7288cc431707aab4',
-    channelAccessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
-});
+//var bot = linebot({
+//    channelId: '1499888713',
+//    channelSecret: '6d53032b1569c18e7288cc431707aab4',
+//    channelAccessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
+//});
 
 //const parser = bodyParser.json({
 //    verify: function (req, res, buf, encoding) {
@@ -23,17 +30,31 @@ var bot = linebot({
 //    }
 //});
 
-//function callback(req, res, next) {
-//    try {
-//        console.log(req.body);
+function callback(req, res, next) {
+    //try {
+    //    console.log(req.body);
 
-//        //var receives = client.createReceivesFromJSON(req.body);
-//    } catch (error) {
-//        console.log(error.message);
-//    }
-//    res.end("ok");
-//    return next();
-//}
+    //    //var receives = client.createReceivesFromJSON(req.body);
+    //} catch (error) {
+    //    console.log(error.message);
+    //}
+
+    const promises = req.body.events.map(event => {
+        // reply message
+        return line.client
+            .replyMessage({
+                replyToken: event.replyToken,
+                messages: [
+                    {
+                        type: 'text',
+                        text: event.message.text
+                    }
+                ]
+            })
+    })
+    Promise.all(promises).then(() => res.json({ success: true }));
+
+}
 bot.on('message', function (event) {
     console.log("1234");
     console.log(event); //把收到訊息的 event 印出來看看
@@ -51,6 +72,6 @@ module.exports.route = {
         '/': [test]
     },
     post: {
-        'linewebhook': [linebotParser]
+        'linewebhook': [line.validator.validateSignature(), callback]
     }
 };
