244579a3e291d1a52e73c618094d4b0413b7429c
hiroxu <uesugi0519@gmail.com>
2017-02-07 20:16:47 +0800

    Add files via upload

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -3,18 +3,18 @@ var bodyParser = require('body-parser');
 var _ = require('lodash');
 var request = require('superagent');
 var LineBot = require('line-bot-sdk');
-var client = LineBot.client({
-    channelID: '1499888713',
-    channelSecret: '6d53032b1569c18e7288cc431707aab4',
-    channelToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
-});
-
-
-//var bot = linebot({
-//    channelId: '1499888713',
+//var client = LineBot.client({
+//    channelID: '1499888713',
 //    channelSecret: '6d53032b1569c18e7288cc431707aab4',
-//    channelAccessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
+//    channelToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
 //});
+
+
+var bot = linebot({
+    channelId: '1499888713',
+    channelSecret: '6d53032b1569c18e7288cc431707aab4',
+    channelAccessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU='
+});
 
 //const parser = bodyParser.json({
 //    verify: function (req, res, buf, encoding) {
@@ -23,23 +23,23 @@ var client = LineBot.client({
 //    }
 //});
 
-function callback(req, res, next) {
-    try {
-        console.log(req.body);
+//function callback(req, res, next) {
+//    try {
+//        console.log(req.body);
 
-        //var receives = client.createReceivesFromJSON(req.body);
-    } catch (error) {
-        console.log(error.message);
-    }
-    res.end("ok");
-    return next();
-}
-//bot.on('message', function (event) {
-//    console.log("1234");
-//    console.log(event); //把收到訊息的 event 印出來看看
-//})
+//        //var receives = client.createReceivesFromJSON(req.body);
+//    } catch (error) {
+//        console.log(error.message);
+//    }
+//    res.end("ok");
+//    return next();
+//}
+bot.on('message', function (event) {
+    console.log("1234");
+    console.log(event); //把收到訊息的 event 印出來看看
+})
 
-//const linebotParser = bot.parser();
+const linebotParser = bot.parser();
 
 function test(req, res, next) {
     res.end("you got mail...");
@@ -51,6 +51,6 @@ module.exports.route = {
         '/': [test]
     },
     post: {
-        'linewebhook': [callback]
+        'linewebhook': [linebotParser]
     }
 };
