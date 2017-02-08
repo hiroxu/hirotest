690a8ae35c1bb92f83616ee84f760f78516d22aa
hiroxu <uesugi0519@gmail.com>
2017-02-08 11:34:23 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -27,5 +27,3 @@ var server = app.listen(process.env.PORT || 8000, function () {
     var port = server.address().port;
     console.log('%s listening at %s port %s', app.name, app.url, port);
 });
-
-console.log(process.env.channelSecret);
