e0fdd55ff78c9965337425eb974a4a1d6911e7de
hiroxu <uesugi0519@gmail.com>
2017-02-08 09:38:29 +0800

    Add files via upload

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -3,13 +3,15 @@ const line = require('node-line-bot-api')
 
 line.init({
     accessToken: 'ODMhVyhYF+NU+UsKMrLtzPfwY6eZVFYchFHwh9uzCc15l4OolmgzSqbHFF0fEKvhzURZJVUb915wpDjCH9P9iBlGcT9VP+pUchc4QfsAyPH/YXybobSwaqjFFkk/MI7Acx9QipbMgm8ktYDaAaFJGAdB04t89/1O/w1cDnyilFU=',
-    // (Optional) for webhook signature validation
     channelSecret: '6d53032b1569c18e7288cc431707aab4'
 })
 
 function callback(req, res, next) {
-    const promises = req.body.events.map(event => {
-        // reply message
+    line.client.getProfile('U55dfe73ed50291498e30fcb60faeb2e7')
+        .then((profile) => {
+            console.log(profile);
+        })
+    const promises = req.body.events.map(event => {    
         console.log(event);
         var message = "無法辨識的訊息";
         if (event.type === 'message') {
@@ -25,10 +27,9 @@ function callback(req, res, next) {
                     text: message
                 }
             ]
-        })           
+        });           
     })
     Promise.all(promises).then(() => res.json({ success: true }));
-
 }
 
 function test(req, res, next) {
